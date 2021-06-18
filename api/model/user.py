368e38b51f8d22425666cf704e2185a48f91ff0e
Anson Lee <a.lee3213@hotmail.com>
2021-06-19 00:38:20 +1000

    Complete setup for backend and frontend

--- api/model/user.py
+++ api/model/user.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Column, Integer, String, DateTime
 
+from api.constants.user import MAX_EMAIL, MAX_PASSWORD, MAX_USERNAME
 from . import db
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(50), unique=True)
-    password = Column(String(100))
-    email = Column(String(255), unique=True, nullable=False)
+    username = Column(String(MAX_USERNAME), unique=True)
+    password = Column(String(MAX_PASSWORD))
+    email = Column(String(MAX_EMAIL), unique=True, nullable=False)
     registered_on = Column(DateTime, nullable=False)
